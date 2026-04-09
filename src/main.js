a5337557b1acdc153f2808629a630d8aa725b9d3
Kevin Fitzhenry <kevinpfitzhenry@gmail.com>
2026-04-09 10:05:08 -0700

    Add animationDelay with JS, remove from CSS

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
+const cardLinks = document.querySelectorAll('.card__list li');
+// console.log(cardLinks);
+
+cardLinks.forEach((item, index) => {
+  // console.log(index, item);
+  
+  let staggerAmount = 200;
+  let animationDelay = index * staggerAmount;
+  
+  // console.log(animationDelay);
+  
+  item.style.animationDelay = animationDelay + 'ms';
+});
 
